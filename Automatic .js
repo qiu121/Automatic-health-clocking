05171dc23d542a47e19a72e02e200252417ac84a
qiu121 <99729305+qiu121@users.noreply.github.com>
2022-04-25 19:51:45 +0800

    Update Automatic .js

--- Automatic .js
+++ Automatic .js
@@ -2,9 +2,9 @@
 // @name            自动健康打卡
 // @name:zh-CN      自动健康打卡
 // @name:zh-TW      自動健康打卡
-// @name:en         Auto-click input
-// @namespace       http://tampermonkey.net/
-// @version         3.0.0
+// @name:en         Automatic-health-clocking
+// @namespace       https://github.com/qiu121/Automatic-health-clocking
+// @version         2022.4
 // @description     基于原生js和简单jQuery的自动健康打卡，实现疫情防控下校园内的每日健康打卡上报
 // @author          qiu121
 // @license         MIT
@@ -20,7 +20,7 @@
 
 //=================================================
 ////此处填写值为有效的用户身份证号
-var id = "0000000000";
+var id = "000000000000000";
 //=================================================
 
 (function() {
@@ -46,21 +46,24 @@ var id = "0000000000";
     //若身份证有效，执行登录操作
     //填入身份证号,待网页渲染完毕执行脚本，进入打卡界面
     window.onload=function(){
-        var btn=document.getElementById("phone");
-        //btn.value=id;
-        $("#phone").val(id);
+        //打卡进程延迟5秒
+        var time_1=setTimeout(function(){
 
-        //提交登录
-        $(".btn_submit").trigger('click');
-        //var btn=document.querySelector('#btn_submit');
-        //var login=document.getElementsByClassName("btn_submit");
+            var btn=document.getElementById("phone");
+            //btn.value=id;
+            $("#phone").val(id);
 
-        //检测是否已经完成打卡
-        var title=document.getElementsByTagName('title');
-        if(title[0].innerHTML=='南昌工学院人员校园出入通行码'|| title[0].innerHTML=='南昌工学院人员电子通行卡' ){
-            window.alert('打卡已完成!');
-        }
-        /*
+            //提交登录
+            $(".btn_submit").trigger('click');
+            //var btn=document.querySelector('#btn_submit');
+            //var login=document.getElementsByClassName("btn_submit");
+
+            //检测是否已经完成打卡
+            var title=document.getElementsByTagName('title');
+            if(title[0].innerHTML=='南昌工学院人员校园出入通行码'|| title[0].innerHTML=='南昌工学院人员电子通行卡' ){
+                window.alert('打卡已完成!');
+            }
+            /*
         window.alert(title.length);
         for(var i=0;i<title.length;i++){
             if(title[i].innerHTML=='南昌工学院人员电子通行卡'){
@@ -68,85 +71,76 @@ var id = "0000000000";
                  break;
             }
         }
-        */             
-        //location.reload();//刷新页面
-        //window.clearInterval(t2)// 去除定时器
-
-        //页面自动刷新       
-        //<meta http-equiv="refresh" content="5">
-
-         //var fresh = setTimeout(function(){
-            //location.reload();
-        //},1500)
-        //休眠，防止进入打卡页面程序因弹出定位警示alert弹窗，中断后续进程执行
-        /*
         var alert=function(){
             return;
         }
         setTimeout(() => console.log(2), 2000);
         */
-        //===========================================================!!!!!!!!!!!!!!!!!
-        //所在地由定位获取，属性为只读，无法修改
+            //===========================================================!!!!!!!!!!!!!!!!!
+            //所在地由定位获取，属性为只读，无法修改
 
-        //健康状况
-        var health=document.getElementById("RadioGroup1_0");
-        $("#RadioGroup1_0").attr("checked",true);
+            //健康状况
+            var health=document.getElementById("RadioGroup1_0");
+            $("#RadioGroup1_0").attr("checked",true);
 
 
-        //是否隔离===================================================！！！！！！！！！
-        //（否：2）（是：1）
-        var separate=document.getElementsByName("vo.gl");
-        //alert(separate.length)
-        if(separate[0].value=="2"){
-            separate[0].checked=true;
-        }
-
-        //近14天是否有中高风险地区旅居史
-        var risk=document.getElementById("RadioGroup6_1");
-        risk.checked=true;
-
-        //是否已接种疫苗===============================================！！！！！！！！！！！
-        // （是：1）（否：2）
-        var vaccinate=document.getElementsByName("vo.jzym")
-        for (var j = 0; j <vaccinate.length; j++) {
-            if (vaccinate[j].value=="1") {
-                var att_2=document.createAttribute("checked");
-                att_2.value=true;
-                vaccinate[j].setAttributeNode(att_2);               
-                break;
+            //是否隔离===================================================！！！！！！！！！
+            //（否：2）（是：1）
+            var separate=document.getElementsByName("vo.gl");
+            //alert(separate.length)
+            if(separate[0].value=="2"){
+                separate[0].checked=true;
             }
-        }
-        //疫苗详情
-        var ymxq =document.getElementById("li_ymxq");
-        //alert(ymxq.style);
-        ymxq.style='display:list-item'//展开疫苗详情
-        /*
+
+            //近14天是否有中高风险地区旅居史
+            var risk=document.getElementById("RadioGroup6_1");
+            risk.checked=true;
+
+            //是否已接种疫苗===============================================！！！！！！！！！！！
+            // （是：1）（否：2）
+            var vaccinate=document.getElementsByName("vo.jzym")
+            for (var j = 0; j <vaccinate.length; j++) {
+                if (vaccinate[j].value=="1") {
+                    var att_2=document.createAttribute("checked");
+                    att_2.value=true;
+                    vaccinate[j].setAttributeNode(att_2);
+                    break;
+                }
+            }
+            //疫苗详情
+            var ymxq =document.getElementById("li_ymxq");
+            //alert(ymxq.style);
+            ymxq.style='display:list-item'//展开疫苗详情
+            /*
             ymxq.style='display:none'
             初始界面，默认折叠
         */
-        var ymxq_1=document.getElementById("ymxq");
-        var ymxq_2=document.getElementsByTagName("option");
-        //alert(ymxq_2.length);
-        //alert(ymxq_2[13].innerHTML);
-        //alert(ymxq_2[13].value);
-        //ymxq_2[ymxq_2.length-1].selected=true;
-        //ymxq_2[13].selected=true;
-        for (var k = 0; k < ymxq_2.length; k++) {
-            if (ymxq_2[k].innerHTML=="接种第三针") {
-                ymxq_2[k].selected=true;
-                break;
+            var ymxq_1=document.getElementById("ymxq");
+            var ymxq_2=document.getElementsByTagName("option");
+            //alert(ymxq_2.length);
+            //alert(ymxq_2[13].innerHTML);
+            //alert(ymxq_2[13].value);
+            //ymxq_2[ymxq_2.length-1].selected=true;
+            //ymxq_2[13].selected=true;
+            for (var k = 0; k < ymxq_2.length; k++) {
+                if (ymxq_2[k].innerHTML=="接种第三针") {
+                    ymxq_2[k].selected=true;
+                    break;
+                }
             }
+
+            //提交数据
+            var time_2=setTimeout(function(){
+                var submit=document.getElementById("my_btn");
+                //$(".btn_submit t_btn").click();
+                //$("#my_btn").trigger('click')
+
+                //没有点击按钮，仅执行了按钮所绑定的事件
+                submit.onclick();
+                //location.reload(); //刷新页面
+            },2000);//延时1.5秒完成提交
+            //window.clearInterval(time_2);
+        },5000)
+
         }
-
-        //提交数据
-        var time_1=setTimeout(function(){
-            var submit=document.getElementById("my_btn");
-            //$(".btn_submit t_btn").click();
-            //$("#my_btn").trigger('click')
-
-            //没有点击按钮，仅执行了按钮所绑定的事件
-            submit.onclick();
-            location.reload(); //刷新页面
-        },1500);//延时1.5秒完成提交
-    }
 })();
